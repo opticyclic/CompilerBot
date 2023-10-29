69edbb2cb9614f3b0dc348da843b2ab860628efc
opticyclic <opticyclic@gmail.com>
2023-10-29 16:19:47 -0400

    ChatGPT: Add initial project

---
+++ src/main/java/com/github/opticyclic/ai/compilerbot/CompilerBotApplication.java
@@ -0,0 +1,11 @@
+package com.github.opticyclic.ai.compilerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilerBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompilerBotApplication.class, args);
+    }
+}
---
+++ src/main/java/com/github/opticyclic/ai/compilerbot/service/FileMonitorService.java
@@ -0,0 +1,8 @@
+package com.github.opticyclic.ai.compilerbot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileMonitorService {
+    // Implement your file monitoring and organizing logic here
+}
