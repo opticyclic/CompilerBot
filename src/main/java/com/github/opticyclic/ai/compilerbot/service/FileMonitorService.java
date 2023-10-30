f3c6f69bcba55b226667ba805650f4c1f50d4c4d
opticyclic <opticyclic@gmail.com>
2023-10-29 22:03:08 -0400

    ChatGPT: Add some of the code back

--- src/main/java/com/github/opticyclic/ai/compilerbot/service/FileMonitorService.java
+++ src/main/java/com/github/opticyclic/ai/compilerbot/service/FileMonitorService.java
@@ -5,9 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 @Service
 public class FileMonitorService {
@@ -18,7 +16,8 @@ public class FileMonitorService {
     @Value("${build.directory}")
     private String buildDirectory;
 
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    @Value("${jdk.location}")
+    private String jdkLocation;
 
     @Scheduled(fixedRate = 10000) // Run every 10 seconds
     public void monitorDirectory() {
@@ -29,11 +28,43 @@ public class FileMonitorService {
         File[] files = sourceDir.listFiles();
         if (files != null) {
             for (File file : files) {
-                if (file.isFile()) {
-                    // Process the file
-                    // You can move or compile files here
+                if (file.isFile() && file.getName().endsWith(".java")) {
+                    String packageName = getPackageName(file);
+                    compileJavaFile(file, packageName);
                 }
             }
+        }
+    }
+
+    private String getPackageName(File file) {
+        // Implement the logic to extract the package name from the Java file
+        // Example logic: Parse the file content to find the package declaration
+        // Replace this with your actual implementation
+        return "com.example"; // Replace with actual package name
+    }
+
+    private void compileJavaFile(File file, String packageName) {
+        // Implement the logic to compile the Java file
+        // Example logic: Use ProcessBuilder to run the Java compiler
+        // Replace this with your actual implementation
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "javac",
+                    "-d", buildDirectory,
+                    "-sourcepath", sourceDirectory,
+                    file.getAbsolutePath()
+            );
+            processBuilder.directory(new File(jdkLocation));
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("Compiled: " + file.getName());
+            } else {
+                System.err.println("Compilation failed: " + file.getName());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
